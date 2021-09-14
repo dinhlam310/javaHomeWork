58d1dc509631dff27864fd7399ce829e6a928d37
dinhlam310 <nnguyendinlamm@gmail.com>
2021-09-14 21:35:37 +0700

    bài tập java buổi 2(1)

---
+++ src/stackjava/cpm/helloworld/Hello.java
@@ -0,0 +1,24 @@
+package stackjava.cpm.helloworld;
+
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       int giaiThua = sc.nextInt();
+       int x = sc.nextInt();
+       for(int i = giaiThua - 1; i >= 1 ;i--) {
+           giaiThua *= i;
+       }
+       for(int y = 1; y < 999; y++){
+           if (Math.pow(x,y) < giaiThua) {
+               if (giaiThua % Math.pow(x, y) == 0) {
+                   y += 1;
+               }
+           } else if (Math.pow(x, y) >= giaiThua) {
+               break;
+           }
+           System.out.println(y);
+       }
+    }
+}
